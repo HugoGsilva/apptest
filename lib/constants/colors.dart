117ce18c22eb93622e87d9ecc13fbe4c45fb222f
hugoartess@gmail.com <bestunter>
2024-10-10 03:08:32 -0300

    Starting a "To Do" app

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFD32F2F);
+const Color tdBlue = Color(0xFF1976D2);
+
+const Color tdBlack = Color(0xFF000000);
+const Color tdGrey = Color(0xFF9E9E9E);
+
+const Color tdBGColor = Color(0xFFE0E0E0);
---
+++ lib/screens/home.dart
@@ -0,0 +1,46 @@
+import 'package:apptest/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+              child: TextField(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Icon(
+          Icons.menu,
+          color: tdBlack,
+          size: 30,
+        ),
+        Container(
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.png')),
+        )
+      ]),
+    );
+  }
+}
